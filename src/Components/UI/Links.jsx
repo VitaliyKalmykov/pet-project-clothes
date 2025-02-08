6507b061d811f4b4e81faa02fa0edf51ecd0ae83
Vitalii <vitaliy.kalmykov.work@gmail.com>
2025-02-08 13:54:08 +0200

    first commit

--- src/Components/UI/Links.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Links = ({href, className, children}) => {
-    return (
-        <a href={href} className={className}>{children}</a>
-    );
-};
-
-export default Links;
